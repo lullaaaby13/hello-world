ce2b8ed8777b29720972700bf528294e4109b01e
lullaaaby13 <sds901234@naver.com>
2023-10-20 09:32:27 +0900

    add HelloController

---
+++ src/main/kotlin/com/example/helloworld/web/HelloController.kt
@@ -0,0 +1,13 @@
+package com.example.helloworld.web
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun getHello(): String {
+        return "Hello World"
+    }
+}
