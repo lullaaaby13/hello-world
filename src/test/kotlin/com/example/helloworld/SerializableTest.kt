037bb5a1dafe9f2c824b3c44531cb1f9d2afffc4
lullaaaby13 <sds901234@naver.com>
2023-10-20 10:28:31 +0900

    github action test 4

--- src/test/kotlin/com/example/helloworld/SerializableTest.kt
+++ src/test/kotlin/com/example/helloworld/SerializableTest.kt
@@ -14,28 +14,5 @@ class SerializableTest {
     @Test
     fun name() {
 
-        // val user = User("admin", "1234")
-        //
-        // val fos = FileOutputStream("user-serialize")
-        // val bos = BufferedOutputStream(fos)
-        // val oos = ObjectOutputStream(bos)
-        // oos.writeObject(user)
-        // oos.close()
-
-        val fis = FileInputStream("user-serialize")
-        val bis = BufferedInputStream(fis)
-        val ois = ObjectInputStream(bis)
-        val read = ois.readObject() as User
-        println(read)
-    }
-}
-
-data class User(
-    val id: String,
-    val password: String,
-    val age: Int,
-) : Serializable {
-    companion object {
-        private const val serialVersionUID: Long = 1L
     }
 }
